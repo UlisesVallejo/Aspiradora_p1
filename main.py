c64d44eb9d97cd5d858419c4fb5ad2da711097d0
ulises vallejo <jose.vallejo7479@alumnos.udg.mx>
2022-10-17 21:49:24 -0500

    practica 1 parte 3

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 import random
-
+ 
 class Agent:
     def __init__(self):
         self.__state = random.choice([0, 1])
         self.__environment = [random.choice([True, False]), random.choice([True, False])]
-        self.__cost = 0
+        self.__battery = 100
         self.__solution = []
-
+ 
     def on(self):
         print("Estado inicial")
         print("Aspiradora en lado:", "izquierdo" if self.__state == 0 else "derecho")
         print("Lado izquierdo:", "sucio" if self.__environment[0] else "limpio")
         print("Lado derecho:", "sucio" if self.__environment[1] else "limpio")
         print("--------------------------------")
-
+ 
         while self.__environment[0] or self.__environment[1]:
-            if self.__environment[self.__state]: # limpiar
+            if self.__environment[self.__state]: # aspirar
                 self.__environment[self.__state] = False
-                self.__cost += 1
+                self.__battery -= 10
                 action = "Aspirar"
-
+ 
             else: # mover
                 if self.__state == 0:
                     self.__state = 1
                     action = "Derecha"
-
+ 
                 else:
                     self.__state = 0
                     action = "Izquierda"
-
-                self.__cost += 1
-
+ 
+                self.__battery -= 5
+ 
             self.__solution.append(action)
-
+ 
             print("Acción:", action)
             print("Aspiradora en lado:", "izquierdo" if self.__state == 0 else "derecho")
             print("Lado izquierdo:", "sucio" if self.__environment[0] else "limpio")
             print("Lado derecho:", "sucio" if self.__environment[1] else "limpio")
-            print("Costo acumulado:", self.__cost)
+            print("Batería:", self.__battery)
             print("--------------------------------")
-
+ 
         print("Acción: NoOp")
         print("Aspiradora en lado:", "izquierdo" if self.__state == 0 else "derecho")
         print("Lado izquierdo:", "sucio" if self.__environment[0] else "limpio")
         print("Lado derecho:", "sucio" if self.__environment[1] else "limpio")
-        print("Costo acumulado:", self.__cost)
-
+        print("Batería:", self.__battery)
+ 
         print("\nSolución:", self.__solution)
-
-
+ 
+ 
 if __name__ == "__main__":
     aspiradora = Agent()
     aspiradora.on()
